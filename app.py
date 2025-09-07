48a52476c8352196fdfc8283456db9f2ad619def
rohit-sanjeev <rohitnhce@gmail.com>
2025-09-07 18:29:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1056,5 +1056,5 @@ def download_excel():
                      mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
